037f75b78e5ef88ce07bde42b7c444b58ab09fe9
miralsuthar <miralsuthar2000@gmail.com>
2023-09-10 19:22:41 +0530

    initial commit

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,32 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
+import "@rainbow-me/rainbowkit/styles.css";
+
+import { getDefaultWallets, RainbowKitProvider } from "@rainbow-me/rainbowkit";
+import { configureChains, createConfig, WagmiConfig } from "wagmi";
+import { polygon } from "wagmi/chains";
+import { publicProvider } from "wagmi/providers/public";
+
+const { chains, publicClient } = configureChains([polygon], [publicProvider()]);
+
+const { connectors } = getDefaultWallets({
+  appName: "phala-flex",
+  projectId: "8f4d88e5110bf5ee4c69dda3302023aa",
+  chains,
+});
+
+const wagmiConfig = createConfig({
+  autoConnect: true,
+  connectors,
+  publicClient,
+});
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <WagmiConfig config={wagmiConfig}>
+      <RainbowKitProvider chains={chains}>
+        <Component {...pageProps} />
+      </RainbowKitProvider>
+    </WagmiConfig>
+  );
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,118 +1,170 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
+import { Inter } from "next/font/google";
+import { ConnectButton } from "@rainbow-me/rainbowkit";
+import QRCode from "react-qr-code";
+import { useCallback, useEffect, useState } from "react";
+import { useAccount } from "wagmi";
+import { supabase } from "@/utils/api";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
+
+const serviceId = process.env.NEXT_PUBLIC_SERVICE_ID as string;
+const AccountSID = process.env.NEXT_PUBLIC_ACCOUNT_SID as string;
+const AuthToken = process.env.NEXT_PUBLIC_AUTH_TOKEN as string;
 
 export default function Home() {
+  const [qruri, setQrui] = useState("");
+  const [factor, setFactor] = useState("");
+  const [isverified, setIsVerified] = useState(false);
+
+  const [isScanningComplete, setIsScanningComplete] = useState(false);
+  const [payload, setPayload] = useState("");
+
+  const [authOtp, setAuthOtp] = useState("");
+
+  const { address } = useAccount();
+
+  const getUri = useCallback(() => {
+    fetch(
+      `https://verify.twilio.com/v2/Services/${serviceId}/Entities/${address}/Factors`,
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/x-www-form-urlencoded",
+          Authorization: "Basic " + btoa(`${AccountSID}:${AuthToken}`),
+        },
+        body: "FriendlyName=Phala flex&FactorType=totp",
+      }
+    )
+      .then((res) => res.json())
+      .then((data) => {
+        console.log("data: ", data);
+        setQrui(data.binding.uri);
+        setFactor(data.sid);
+      });
+  }, [address]);
+
+  const verifyUser = (otp: string) => {
+    fetch(
+      `https://verify.twilio.com/v2/Services/${serviceId}/Entities/${address}/Factors/${factor}`,
+      {
+        method: "POST",
+        headers: {
+          Authorization: "Basic " + btoa(`${AccountSID}:${AuthToken}`),
+        },
+        body: new URLSearchParams({
+          AuthPayload: otp,
+        }),
+      }
+    )
+      .then((res) => res.json())
+      .then(async (data) => {
+        if (data.status === "verified") {
+          setIsVerified(true);
+          console.log("this is working");
+          const { error } = await supabase.from("user").insert({
+            address: address as string,
+            authy_id: 1,
+            factor: factor,
+            factor_hash: factor,
+          });
+        }
+      });
+  };
+
+  const verifyToken = (otp: string, factor: string) => {
+    fetch(
+      `https://verify.twilio.com/v2/Services/${serviceId}/Entities/${address}/Challenges`,
+      {
+        method: "POST",
+        headers: {
+          Authorization: "Basic " + btoa(`${AccountSID}:${AuthToken}`),
+        },
+        body: new URLSearchParams({
+          AuthPayload: otp,
+          FactorSid: factor,
+        }),
+      }
+    );
+  };
+
+  useEffect(() => {
+    (async () => {
+      const { data, error } = await supabase
+        .from("user")
+        .select("*")
+        .eq("address", address);
+      if (address && data?.length === 0 && !isScanningComplete) {
+        getUri();
+      }
+    })();
+  }, [address, getUri, isScanningComplete]);
+
   return (
     <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
+      className={`flex bg-white text-gray-600 min-h-screen flex-col items-center p-24 gap-20 ${inter.className}`}
     >
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/pages/index.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+      <ConnectButton />
+      {!isScanningComplete && address && qruri && (
+        <div className="flex flex-col items-center gap-10">
+          <p>
+            Enable 2FA for this deposit contract scan this qr code with your
+            choice of authenticator app.
+          </p>
+          <QRCode value={qruri} />
+          <button
+            className="bg-blue-500 px-3 py-1 text-white rounded-md text-lg w-max hover:scale-105 transition-all"
+            onClick={() => setIsScanningComplete(true)}
           >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+            Done
+          </button>
         </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
+      )}
+      <div>
+        {!isverified && isScanningComplete && (
+          <div className="flex flex-col items-center gap-10">
+            <p>
+              Enter the code displaying on your authenticator app.(Ex. 467 890)
+            </p>
+            <input
+              value={payload}
+              onChange={(e) => setPayload(e.target.value)}
+              placeholder="otp"
+              className="text-black text-center text-2xl rounded-md border p-1 border-gray-500"
+            />
+            <button
+              onClick={() => verifyUser(payload)}
+              className="bg-blue-500 text-white px-3 py-1 rounded-md text-lg w-max hover:scale-105 transition-all"
+            >
+              Check
+            </button>
+          </div>
+        )}
+        {isverified && (
+          <>
+            <span className="text-center text-green-500 text-2xl">
+              Verification Successful!
             </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Discover and deploy boilerplate example Next.js&nbsp;projects.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+            <div className="flex flex-col items-center gap-10">
+              <p>
+                Enter the code displaying on your authenticator app.(Ex. 467
+                890)
+              </p>
+              <input
+                value={authOtp}
+                onChange={(e) => setAuthOtp(e.target.value)}
+                placeholder="otp"
+                className="text-black text-center text-2xl rounded-md border p-1 border-gray-500"
+              />
+              <button
+                onClick={() => verifyToken(authOtp, factor)}
+                className="bg-blue-500 text-white px-3 py-1 rounded-md text-lg w-max hover:scale-105 transition-all"
+              >
+                Check
+              </button>
+            </div>
+          </>
+        )}
       </div>
     </main>
-  )
+  );
 }
