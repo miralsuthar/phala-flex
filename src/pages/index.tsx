bddaf84d3bb6193d85ec04f2d2488d55bb3f1c24
miralsuthar <miralsuthar2000@gmail.com>
2023-09-11 13:19:15 +0530

    feat: add owner and beneficiery selector

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,6 +4,7 @@ import QRCode from "react-qr-code";
 import { useCallback, useEffect, useState } from "react";
 import { useAccount } from "wagmi";
 import { supabase } from "@/utils/api";
+import { cn } from "@/utils/helpers";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,10 +17,14 @@ export default function Home() {
   const [factor, setFactor] = useState("");
   const [isverified, setIsVerified] = useState(false);
 
+  const [account, setAccount] = useState();
+
   const [isScanningComplete, setIsScanningComplete] = useState(false);
   const [payload, setPayload] = useState("");
 
-  const [authOtp, setAuthOtp] = useState("");
+  const [step, setStep] = useState<number>(1);
+  const [userType, setUserType] = useState<"owner" | "beneficiery">();
+  const [activeField, setActiveField] = useState("deposit");
 
   const { address } = useAccount();
 
@@ -63,7 +68,6 @@ export default function Home() {
           console.log("this is working");
           const { error } = await supabase.from("user").insert({
             address: address as string,
-            authy_id: 1,
             factor: factor,
             factor_hash: factor,
           });
@@ -95,6 +99,8 @@ export default function Home() {
         .eq("address", address);
       if (address && data?.length === 0 && !isScanningComplete) {
         getUri();
+      } else {
+        setAccount(data?.[0]);
       }
     })();
   }, [address, getUri, isScanningComplete]);
@@ -144,25 +150,78 @@ export default function Home() {
             <span className="text-center text-green-500 text-2xl">
               Verification Successful!
             </span>
-            <div className="flex flex-col items-center gap-10">
-              <p>
-                Enter the code displaying on your authenticator app.(Ex. 467
-                890)
-              </p>
-              <input
-                value={authOtp}
-                onChange={(e) => setAuthOtp(e.target.value)}
-                placeholder="otp"
-                className="text-black text-center text-2xl rounded-md border p-1 border-gray-500"
-              />
-              <button
-                onClick={() => verifyToken(authOtp, factor)}
-                className="bg-blue-500 text-white px-3 py-1 rounded-md text-lg w-max hover:scale-105 transition-all"
-              >
-                Check
-              </button>
-            </div>
           </>
+        )}
+        {account && step === 1 && (
+          <div className="flex flex-col justify-center items-center gap-5">
+            <span>Select the type of account</span>
+            <div className="flex gap-10">
+              <div
+                onClick={() => setUserType("owner")}
+                className={cn(
+                  "h-20 w-28 bg-gray-200 rounded-lg cursor-pointer hover:bg-blue-100 hover:border hover:border-blue-300 transition-all flex justify-center items-center",
+                  userType === "owner" && "bg-blue-300"
+                )}
+              >
+                Owner
+              </div>
+              <div
+                onClick={() => setUserType("beneficiery")}
+                className={cn(
+                  "h-20 w-28 bg-gray-200 rounded-lg cursor-pointer hover:bg-blue-100 hover:border hover:border-blue-300 transition-all flex justify-center items-center",
+                  userType === "beneficiery" && "bg-blue-300"
+                )}
+              >
+                beneficiery
+              </div>
+            </div>
+            <button
+              onClick={() => setStep(2)}
+              className="bg-blue-500 text-white px-3 py-1 rounded-lg text-xl hover:scale-105 transition-all"
+            >
+              continue
+            </button>
+          </div>
+        )}
+        {account && step === 2 && (
+          <div className="flex flex-col justify-center items-center gap-5">
+            <div className="flex bg-gray-100 rounded-lg relative">
+              <span
+                className="w-28 text-center p-2 cursor-pointer"
+                onClick={() => setActiveField("deposit")}
+              >
+                Deposit
+              </span>
+              <span
+                className="w-28 text-center p-2 cursor-pointer"
+                onClick={() => setActiveField("reedem")}
+              >
+                reedem
+              </span>
+              <div
+                className={cn(
+                  "w-28 h-full absolute left-0 top-0 bg-blue-400 rounded-lg opacity-10 border-2 border-blue-800 transition-all",
+                  activeField === "reedem" && "left-28"
+                )}
+              ></div>
+            </div>
+
+            <div className="space-x-4">
+              <input
+                className="p-2 border-2 border-gray-300 rounded-lg"
+                placeholder="value"
+                type="text"
+              />
+              <select
+                defaultValue="matic"
+                className="p-2 border-2 border-gray-300 rounded-lg cursor-pointer"
+              >
+                <option value="usdc">USDC</option>
+                <option value="usdt">USDT</option>
+                <option value="matic">MATIC</option>
+              </select>
+            </div>
+          </div>
         )}
       </div>
     </main>
