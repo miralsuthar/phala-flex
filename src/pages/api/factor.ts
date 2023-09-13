288c6505a6eafdb3b6b08faab3e2d2912d5f8fee
Keshav Sharma <skeshav0825@gmail.com>
2023-09-13 19:25:02 +0530

    added all contract-related hooks & contract ABI

--- src/pages/api/factor.ts
+++ src/pages/api/factor.ts
@@ -4,35 +4,35 @@ import { supabase } from "@/utils/db";
 import { requireAuthentication } from "@/utils/middleware";
 
 export type ResponseType = {
-  code: number;
-  factor?: string;
-  error?: string;
+	code: number;
+	factor?: string;
+	error?: string;
 };
 
 export default async function handler(
-  req: NextApiRequest,
-  res: NextApiResponse<ResponseType>
+	req: NextApiRequest,
+	res: NextApiResponse<ResponseType>
 ) {
-  requireAuthentication(req, res, async () => {
-    const { factorHash } = req.query;
+	requireAuthentication(req, res, async () => {
+		const { factorHash } = req.query;
 
-    const data = await supabase
-      .from("user")
-      .select("factor")
-      .filter("factor_hash", "eq", factorHash);
+		const data = await supabase
+			.from("user")
+			.select("factor")
+			.filter("factor_hash", "eq", factorHash);
 
-    const factor = data?.data?.[0]?.factor;
+		const factor = data?.data?.[0]?.factor;
 
-    if (factor != undefined) {
-      return res.status(200).send({
-        code: 200,
-        factor,
-      });
-    }
+		if (factor != undefined) {
+			return res.status(200).send({
+				code: 200,
+				factor,
+			});
+		}
 
-    return res.status(404).send({
-      code: 404,
-      error: "Factor Not Found.",
-    });
-  });
+		return res.status(404).send({
+			code: 404,
+			error: "Factor Not Found.",
+		});
+	});
 }
--- src/utils/sendFaucet.ts
+++ src/utils/sendFaucet.ts
@@ -4,34 +4,34 @@ import { privateKeyToAccount } from "viem/accounts";
 import { polygonMumbai } from "viem/chains";
 
 export const sendFaucet = async (address: string, amount: number) => {
-  const account = privateKeyToAccount(
-    process.env.NEXT_PUBLIC_PRIVATE_KEY_DUSD_FAUCET as `0x${string}`
-  );
+	const account = privateKeyToAccount(
+		process.env.NEXT_PUBLIC_PRIVATE_KEY_DUSD_FAUCET as `0x${string}`
+	);
 
-  const client = createPublicClient({
-    chain: polygonMumbai,
-    transport: http(),
-  });
+	const client = createPublicClient({
+		chain: polygonMumbai,
+		transport: http(),
+	});
 
-  const walletClient = createWalletClient({
-    account,
-    chain: polygonMumbai,
-    transport: http(),
-  });
+	const walletClient = createWalletClient({
+		account,
+		chain: polygonMumbai,
+		transport: http(),
+	});
 
-  const { request } = await client.simulateContract({
-    account,
-    address: "0xFBA3912Ca04dd458c843e2EE08967fC04f3579c2",
-    abi: dUSD,
-    functionName: "transfer",
-    args: [],
-  });
+	const { request } = await client.simulateContract({
+		account,
+		address: "0xFBA3912Ca04dd458c843e2EE08967fC04f3579c2",
+		abi: dUSD,
+		functionName: "transfer",
+		args: [],
+	});
 
-  const tx = await walletClient.writeContract(request);
+	const tx = await walletClient.writeContract(request);
 
-  const transaction = await client.waitForTransactionReceipt({
-    hash: tx,
-  });
+	const transaction = await client.waitForTransactionReceipt({
+		hash: tx,
+	});
 
-  return transaction;
+	return transaction;
 };
