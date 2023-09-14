fd4d2571ae7776f98f30db0c9cecbf8ae0028439
miralsuthar <miralsuthar2000@gmail.com>
2023-09-14 17:56:54 +0530

    fix: change contract address

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -1,2 +1,2 @@
-export const contractAddress = "0x3C8a8eB187ced0c60f9769B4F703b82A9329df1e";
-export const dUSDAddress = "0x2882CE9eC73cd80AB6c048C030BDa65fd3A0263A";
+export const contractAddress = "0x748B2e407D720E5a1a53e0B3eC37cf669B8C6ebE";
+export const dUSDAddress = "0x203FCc4de1768769a96CF0142ADf9Cf9d139510B";
